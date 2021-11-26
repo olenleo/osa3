b0560b69826d4d67dd8d4cd44c2460685fd551d7
Leo Niemi <niemi.leo@gmail.com>
2021-11-26 14:36:45 +0200

    3.15-3.17* - backend delete, error handler middleware, backend update

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -13,6 +13,8 @@ const morgan = require('morgan')
 morgan.token('json-content', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url [:status] :response-time :json-content'))
 
+
+
 app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
@@ -59,20 +61,48 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+    
   })
 
 
 app.put('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = request.body
-  console.log(person)
-  response.json(person)
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+    }
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
 })
 
+
+const unknownEndpoint= (request, response) => {
+  response.status(404).send({ error: 'Unkown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
